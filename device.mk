6fb3573a47cfb0043d3b24f5c44d14f0ddea2123
George <geoster3d@gmail.com>
2021-08-17 13:02:25 +0300

    phoenix: Use prebuilt audio HAL

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
-    audio.primary.sdm710 \
     audio.r_submix.default \
     audio.usb.default \
     libaudiopreprocessing \

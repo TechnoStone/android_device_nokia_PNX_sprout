1d491b96c1bc9b57fb5a0095003c96ed44806a9b
George <geoster3d@gmail.com>
2021-08-17 13:05:25 +0300

    phoenix: Build renderscript HAL

--- device.mk
+++ device.mk
@@ -391,6 +391,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \

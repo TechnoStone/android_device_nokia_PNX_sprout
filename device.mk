784f25c30d69996667e1fb7e9a6d93bb79c583b9
Bladeyy <getsuga369@gmail.com>
2021-11-25 08:22:37 +0530

    Phoenix: Build soundtrigger@2.1 hidl hal

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.sdm710 \
     audio.r_submix.default \

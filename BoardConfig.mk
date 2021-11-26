708c66fc5b0448b4fbcd4e97b26f03734856a322
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-26 14:40:14 +0530

    Phoenix: Simplify AVB flag logic * AVB's `--flags 3` argument builds a disabled VBMeta with   disabled hashtree, so no need to set both flags individually.
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,8 +126,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Inherit from the proprietary version
 include vendor/nokia/Phoenix/BoardConfigVendor.mk

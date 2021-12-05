539be2c600e78ceb66e93fc43d7784120ca893be
Zinadin Zidan <zidan.roking@gmail.com>
2021-12-05 15:34:27 +0530

    Phoenix: rootdir: Import init.recovery.qcom rc Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com> Change-Id: I78c8e91f573ca8512e96cd7e76f46e220fccfe07

--- device.mk
+++ device.mk
@@ -203,6 +203,7 @@ PRODUCT_PACKAGES += \
     init.pnx.target.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
+    init.recovery.qcom.rc \
     init.target.rc \
     fstab.qcom \
     ueventd.qcom.rc
